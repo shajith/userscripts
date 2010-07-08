77bd580c54b1eb6d2506bf734ec54a8364a91b15
Shajith Chacko <shajith@fastpencil.com>
2010-07-08 12:08:20 -0700

    Making this a project. Was: https://gist.github.com/d88309a6b12c61c4d345

---
+++ techmemefilter.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name           Tech Meme filter
+// @namespace      bookseeker
+// @description    Remove iphone/android news from Techmeme
+// @include        http://www.techmeme.com/*
+// ==/UserScript==
+
+arr = document.querySelectorAll('div.item, div.heditem');
+
+for(i=0;i<arr.length;i++){
+    if(arr[i].innerHTML && (arr[i].innerHTML.match(/Android|iPod|iPhone|iOS|Droid/))){
+	arr[i].parentNode.removeChild(arr[i]);
+    }
+}
