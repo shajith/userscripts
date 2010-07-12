312ac9781711f988d6c134dca3914d63e199b2f6
Shajith Chacko <shajith@fastpencil.com>
2010-07-12 16:42:27 -0700

    Yet Another Youtube Cleanup Userscript.

---
+++ youtubeplain.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name           Tech Meme filter
+// @namespace      http://github.com/shajith
+// @description    Remove everything but title and video from youtube video pages.
+// @include        http://www.youtube.com/watch/*
+// ==/UserScript==
+
+arr = document.querySelectorAll('#pagetop, #pagebottom, #watch-main-container')
+
+for(i=0;i<arr.length;i++){
+    arr[i].parentNode.removeChild(arr[i]);
+}
