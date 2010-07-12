42f7395f3acad6022c2603fc72fc5a62af62b8e3
Shajith Chacko <shajith@fastpencil.com>
2010-07-12 16:45:24 -0700

    Corrected URL pattern and title in metadata.

--- youtubeplain.user.js
+++ youtubeplain.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name           Tech Meme filter
+// @name           Youtube Plain
 // @namespace      http://github.com/shajith
 // @description    Remove everything but title and video from youtube video pages.
-// @include        http://www.youtube.com/watch/*
+// @include        http://www.youtube.com/watch*
 // ==/UserScript==
 
 arr = document.querySelectorAll('#pagetop, #pagebottom, #watch-main-container')
