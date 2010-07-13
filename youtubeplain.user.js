e03f60a7ec2b31569fb239b62edbb6be1c3cc011
Shajith Chacko <shajith@fastpencil.com>
2010-07-12 17:31:57 -0700

    Better-er.

--- youtubeplain.user.js
+++ youtubeplain.user.js
@@ -1,5 +1,5 @@
 var _toggleCrap = function (hide) {
-    arr = document.querySelectorAll('#pagetop, #pagebottom, #watch-main-container')
+    arr = document.querySelectorAll('#pagetop, #pagebottom, #watch-main-container, #watch-headline-user-info')
 
     for(i=0;i<arr.length;i++){
 	if(hide)
@@ -14,6 +14,6 @@ d=document.createElement("div");
 
 _toggleCrap(true);
 
-d.innerHTML = '<button type="button" class="yt-uix-button" onclick="arr=document.querySelectorAll(\'#pagetop, #pagebottom, #watch-main-container\');for(i=0;i<arr.length;i++){arr[i].style.display = \'\';};document.getElementById(\'showUIButton\').style.display=\'none\';return false;" id="showUIButton" style=""><span class="yt-uix-button-content">Show UI</span></button>';
+d.innerHTML = '<button type="button" class="yt-uix-button" onclick="arr=document.querySelectorAll(\'#pagetop, #pagebottom, #watch-main-container, #watch-headline-user-info\');for(i=0;i<arr.length;i++){arr[i].style.display = \'\';};document.getElementById(\'showUIButton\').style.display=\'none\';return false;" id="showUIButton" style=""><span class="yt-uix-button-content">Show UI</span></button>';
 
-document.getElementById("watch-headline-user-info").appendChild(d.children[0]);
+document.getElementById("watch-headline-user-info").parentNode.appendChild(d);
