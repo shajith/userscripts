e5a5c571dc03b882d9a059d69c7c2c1e2d2c9acb
Shajith Chacko <shajith@fastpencil.com>
2010-07-12 17:23:04 -0700

    This is better.

--- youtubeplain.user.js
+++ youtubeplain.user.js
@@ -1,12 +1,19 @@
-// ==UserScript==
-// @name           Youtube Plain
-// @namespace      http://github.com/shajith
-// @description    Remove everything but title and video from youtube video pages.
-// @include        http://www.youtube.com/watch*
-// ==/UserScript==
+var _toggleCrap = function (hide) {
+    arr = document.querySelectorAll('#pagetop, #pagebottom, #watch-main-container')
 
-arr = document.querySelectorAll('#pagetop, #pagebottom, #watch-main-container')
+    for(i=0;i<arr.length;i++){
+	if(hide)
+	    arr[i].style.display = "none";
+	else
+	    arr[i].style.display = "";
+    }
 
-for(i=0;i<arr.length;i++){
-    arr[i].parentNode.removeChild(arr[i]);
 }
+
+d=document.createElement("div");
+
+_toggleCrap(true);
+
+d.innerHTML = '<button type="button" class="yt-uix-button" onclick="arr=document.querySelectorAll(\'#pagetop, #pagebottom, #watch-main-container\');for(i=0;i<arr.length;i++){arr[i].style.display = \'\';};document.getElementById(\'showUIButton\').style.display=\'none\';return false;" id="showUIButton" style=""><span class="yt-uix-button-content">Show UI</span></button>';
+
+document.getElementById("watch-headline-user-info").appendChild(d.children[0]);
