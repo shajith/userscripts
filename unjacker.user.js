bd1f40f9fde28dea793240d0c48ff209ba4b7578
Shajith Chacko <shajith@fastpencil.com>
2010-08-28 18:22:55 -0700

    Made the un-framejacking script handle StumbleUpon frame-jacking as well.

--- unjacker.user.js
+++ unjacker.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
-// @name           Hootsuite Un-jacker
+// @name           iFrame Un-jacker
 // @namespace      http://github.com/shajith
-// @description    Kill Hootsuite's framejacking. Why: http://www.informationweek.com/788/88uwdw.htm
+// @description    Kill framejacking by HootSuite and StumbleUpon. Why: http://www.informationweek.com/788/88uwdw.htm
 // @include        http://ht.ly/*
+// @include        http://www.stumbleupon.com/*
 // ==/UserScript==
 
 theFrame = document.querySelectorAll('iframe')[0];
